e15e76ad91b9ef0a86e6ebcfe28269058a8811d1
Leonidas <augustoamaro@icloud.com>
2021-08-13 11:39:02 -0300

    Added HTTTP Requests XMLHttpRequest

---
+++ 16 - HTTP Requets/Json&Xml.js
@@ -0,0 +1,17 @@
+//JSON
+
+// {
+//     "name":{
+//        "firstName":"Colt",
+//        "lastName":"Steele"
+//     },
+//     "email":"coltsteele@gmail.com"
+//  }
+
+
+//XML
+
+<name>
+    <firstName>Colt</firstName>
+    <lastName>Steele</lastName>
+</name>

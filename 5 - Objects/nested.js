da8d9e259de2e44de952072839a0b813dbf00f18
unknown <augustoamaro@icloud.com>
2021-08-14 20:29:38 -0300

    Objects examples

---
+++ 5 - Objects/nested.js
@@ -0,0 +1,31 @@
+const student = {
+    firstName: 'David',
+    lastName: 'Jones',
+    strengths: ['Music', 'Art'],
+    exams: {
+        midterm: 92,
+        final: 88
+    }
+};
+
+const avg = (student.exams.midterm + student.exams.final) / 2
+console.log(avg);
+
+
+const shoppingCart = [
+    {
+        product: 'PS5',
+        price: 499,
+        qty: 1,
+    },
+    {
+        product: 'Game Pass',
+        price: 19,
+        qty: 5,
+    },
+    {
+        product: 'PS5 Controller',
+        price: 199.90,
+        qty: 1,
+    }
+]
---
+++ 5 - Objects/objects.js
@@ -0,0 +1,10 @@
+
+const fitBitData = {
+    totalSteps: 3034,
+    totalMiles: 12.8,
+    avgCalorieBurn: 234,
+    workoutsThisWeek: '5 of 7',
+    avgGoodSleep: '2:13'
+};
+
+console.log(fitBitData.totalSteps);

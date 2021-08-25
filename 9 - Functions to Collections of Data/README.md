8c8d1776c6d8201eab93c5311f89fc0832d53795
Leonidas <augustoamaro@icloud.com>
2021-08-25 11:28:30 -0300

    Added functions to collections of data section

---
+++ 9 - Functions to Collections of Data/README.md
@@ -0,0 +1,10 @@
+
+Methods:
+
+forEach
+map
+filter
+find 
+reduce
+some
+every
---
+++ 9 - Functions to Collections of Data/arrowFunctions.js
@@ -0,0 +1,13 @@
+// const square = function(x) {
+//     return x * x;
+// }
+
+const square = (x) => {
+    return x * x;
+}
+
+const isEven = (num) => {
+    return num % 2 === 0;
+}
+
+console.log(isEven(4))

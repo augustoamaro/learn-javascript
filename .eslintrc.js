a06d591da96e03edd37bdb9a8359ef0853d1ecf5
Augusto Amaro <augustoamaro@icloud.com>
2022-02-11 17:08:14 -0300

    Config ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,23 @@ module.exports = {
     "es2021": true
   },
   "extends": [
-    "airbnb-base"
+    "airbnb-base",
+    "prettier"
   ],
   "parserOptions": {
     "ecmaVersion": "latest"
   },
+  "plugins": [
+    "prettier"
+  ],
   "rules": {
-    "semi": [2, "always"]
+    // "semi": ["error", "always"],
+    "prettier/prettier": "error",
+    "no-unused-vars": "warn",
+    "no-console": "off",
+    "func-names": "off",
+    "no-process-exit": "off",
+    "object-shorthand": "off",
+    "class-methods-use-this": "off"
   }
 }

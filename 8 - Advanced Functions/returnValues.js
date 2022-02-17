72e38be24e60d2fe6e5d577c02af0e9224708e5a
Augusto <augustoamaro@icloud.com>
2022-02-16 21:57:43 -0300

    Added function factory

--- 8 - Advanced Functions/returnValues.js
+++ 8 - Advanced Functions/returnValues.js
@@ -1,3 +1,5 @@
+// Functions factory
+
 // Esssa função retorna uma função.
 function multiplyBy(num) {
     return function (x) {
@@ -5,35 +7,36 @@ function multiplyBy(num) {
     };
   }
   
-  //triple holds a function:
+  //triple armazena uma função:
   const triple = multiplyBy(3);
-  //we can call it:
-  triple(4); //12
-  triple(10); //30
+  // pode ser chamada
+  triple(4); // 12
+  triple(10); // 30
   
   const double = multiplyBy(2);
-  double(3); //6
-  double(9); //18
+  double(3); // 6
+  double(9); // 18
   
   const halve = multiplyBy(0.5);
-  halve(5); //2.5
-  halve(100); //50
+  halve(5); // 2.5
+  halve(100); // 50
   
-  // This function also acts as a "function factory"
+  // Essa função também é uma "function factory"
   function makeBetweenFunc(x, y) {
     return function (num) {
       return num >= x && num <= y;
     };
   }
-  // This function checks if a value is between 0 and 18
+  
+  // Essa função chega se o valor está entre 0 e 18.
   const isChild = makeBetweenFunc(0, 18);
-  isChild(10); //true
-  isChild(56); //false
+  isChild(10); // true
+  isChild(56); // false
   
   const isInNineties = makeBetweenFunc(1990, 1999);
-  isInNineties(1994); //true
-  isInNineties(1987); //false
+  isInNineties(1994); // true
+  isInNineties(1987); // false
   
   const isNiceWeather = makeBetweenFunc(60, 79);
-  isNiceWeather(68); //true
-  isNiceWeather(98); //false
+  isNiceWeather(68); // true
+  isNiceWeather(98); // false

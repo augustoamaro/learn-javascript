a2e32b12a6f2fad6b49694348b034471e8eae8b4
Augusto Amaro <augustoamaro@icloud.com>
2022-02-22 14:43:35 -0300

    Added dom examples

--- 12 - Twisting DOM/index.js
+++ 12 - Twisting DOM/index.js
@@ -3,6 +3,7 @@ const h1_tag = document.querySelector('h1')
 const h2_tag = document.querySelector('h2')
 const input = document.querySelectorAll('input')
 const lis = document.querySelectorAll('li')
+const section = document.querySelector('section')
 console.log(input)
 
 p_tag.innerText = 'OH YEAH'
@@ -15,3 +16,14 @@ input[1].setAttribute('min', '0')
 for (let i = 0; i < lis.length; i++) {
   lis[i].innerText = 'APERTA A BRABA'
 }
+
+h1_tag.style.color = 'yellow';
+h1_tag.style.fontWeight = 'bold'
+h1_tag.style.backgroundColor = 'red';
+
+const novo_h1 = document.createElement('h1')
+
+novo_h1.innerText = 'H1 criado'
+section.appendChild(novo_h1)
+
+//removeChild, remove

bb2b52ed11219ccbc7084ad997395a08b9c2c16a
Augusto <augustoamaro@icloud.com>
2022-02-24 22:14:04 -0300

    Crazy Button

--- 15 - Events/events.js
+++ 15 - Events/events.js
@@ -2,6 +2,13 @@ const btn = document.querySelector('[btn]')
 
 btn.addEventListener('mouseover', function() {
   console.log('passou')
-  btn.style.left = '200px'
-  btn.style.top = '100px'
+  const heigth = Math.floor(Math.random() * window.innerHeight)
+  const width = Math.floor(Math.random() * window.innerWidth)
+  btn.style.left = `${width}px`;
+  btn.style.top = `${heigth}px`;
+});
+
+btn.addEventListener('click', function() {
+  btn.innerText = 'Clicou'
+  document.body.style.backgroundColor = 'green';
 })

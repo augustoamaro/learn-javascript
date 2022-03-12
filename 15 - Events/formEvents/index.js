9393e9874ff6a0346d76b3d4bd64fd63dba052a3
Augusto <augustoamaro@icloud.com>
2022-03-12 16:12:41 -0300

    Finished events examples

--- 15 - Events/formEvents/index.js
+++ 15 - Events/formEvents/index.js
@@ -1,6 +1,14 @@
 const form = document.querySelector('#signup-form');
 
+const creditCardInput = document.querySelector('#cc');
+const termsCheckbox = document.querySelector('#terms');
+const veggieSelect = document.querySelector('#veggie');
+
+
 form.addEventListener('submit', function(e) {
   alert('submited');
+  console.log('cc', creditCardInput.value)
+  console.log('terms', termsCheckbox.checked)
+  console.log('veggie', veggieSelect.value)
   e.preventDefault()
 })

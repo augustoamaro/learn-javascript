eb6874b27cfaa82bf49b66c10aa79eb4df5ecd48
Augusto <augustoamaro@icloud.com>
2022-04-16 23:38:25 -0300

    Added async/await examples

--- 18 - Async-Await/async-await.js
+++ 18 - Async-Await/async-await.js
@@ -2,8 +2,20 @@
 // Await - Podemos usar await somente dentro de funções. Irá pausar a execução da função, esperar por uma promessa ser resolvida.
 
 const getData = async () => {
-    const data = await axios.get('https://swapi.dev/api/planets')
-    console.log(data)
+    const data = await axios.get('https://swapi.dev/api/planets');
+    console.log(data);
 }
 
-getData()
+getData();
+
+
+async function get3Pokemon() {
+    const promise1 = axios.get('http://pokeapi.co/api/v2/pokemon/1');
+    const promise2 = axios.get('http://pokeapi.co/api/v2/pokemon/2');
+    const promise3 = axios.get('http://pokeapi.co/api/v2/pokemon/3 ');
+
+    const results = await Promise.all([promise1, promise2, promise3]);
+    console.log(results);
+}
+
+get3Pokemon();
